890c0f3c5fd12eb9b13a3fc926b8bde84a19a5be
Thiago Pereira Alves <tpereiraalves2013@outlook.com>
2023-06-02 21:45:29 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const btnPesquisarCEP = document.querySelector("#btnPesquisar");
+const inputCep = document.querySelector("#cep");
+
+const erro = document.querySelector(".erro");
+
+
+btnPesquisarCEP.addEventListener("click", event => {
+    event.preventDefault();
+    const valorCEP = inputCep.value;
+    console.log(valorCEP.length);
+    if (valorCEP.length == 0) {
+
+        inputCep.style.border = "2px solid red";
+        erro.innerText = "Campo obrigatório";
+
+        return false;
+    }
+
+    //Consumindo a API do  ViaCep
+    const url = `https://viacep.com.br/ws/${valorCEP}/json/`;
+
+    fetch(url);
+
+    fetch(url).then(response => {
+        return response.json();
+
+    }).then(data => {
+        atribuitCampos(data);
+    }).catch(erro => {
+        alert("CEP inválido");
+    });
+
+}
+
+);
+
+
+function atribuitCampos(data) {
+    const rua = document.querySelector("#rua");
+    const complemento = document.querySelector("#complemento");
+    const bairro = document.querySelector("#bairro");
+    const cidade = document.querySelector("#cidade");
+    const estado = document.querySelector("#estado");
+
+
+    rua.value = data.logradouro;
+    complemento.value = data.complemento;
+    bairro.value = data.bairro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+
+}
+
+function limparCampoCEP() {
+    inputCep.style.border = "none";
+    erro.innerText = "";
+}
